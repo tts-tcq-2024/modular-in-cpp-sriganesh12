8400ec1a28fe559da26f4a5d5da2c4a68a21b6e2
sriganesh12 <49653366+sriganesh12@users.noreply.github.com>
2024-09-24 20:43:11 +0530

    Create colorPair.h

---
+++ colorPair.h
@@ -0,0 +1,20 @@
+#ifndef COLORPAIR_H
+#define COLORPAIR_H
+
+#include "colorsMajorMinor.h"
+
+namespace TelCoColorCoder 
+{
+    class ColorPair 
+    {
+    private:
+        MajorColor majorColor;
+        MinorColor minorColor;
+    public:
+        ColorPair(MajorColor major, MinorColor minor);
+        MajorColor getMajor();
+        MinorColor getMinor();
+    };
+}
+
+#endif
