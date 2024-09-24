f0d8f75ddc32290aee0593127c70e66076c2731b
sriganesh12 <49653366+sriganesh12@users.noreply.github.com>
2024-09-24 20:07:21 +0530

    Create colorsMajorMinor.h

---
+++ colorsMajorMinor.h
@@ -0,0 +1,27 @@
+#ifndef COLORSMAJORMINOR_H
+#define COLORSMAJORMINOR_H
+
+namespace TelCoColorCoder 
+{
+    enum MajorColor : int
+    {
+    WHITE = 0, 
+    RED = 1,
+    BLACK = 2,
+    YELLOW = 3,
+    VIOLET = 4
+    };
+    enum MinorColor : int
+    {
+    BLUE = 0,
+    ORANGE = 1,
+    GREEN = 2,
+    BROWN = 3,
+    SLATE = 4
+    };
+
+    const int numberOfMajorColors{5};
+    const int numberOfMinorColors{5};
+}
+
+#endif
