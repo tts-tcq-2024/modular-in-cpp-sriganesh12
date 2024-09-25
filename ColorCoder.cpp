f1d44fe447a66f61e3dd2ef841b83b2e8edfa4d5
sriganesh12 <49653366+sriganesh12@users.noreply.github.com>
2024-09-25 19:09:08 +0530

    Update ColorCoder.cpp

--- ColorCoder.cpp
+++ ColorCoder.cpp
@@ -6,8 +6,8 @@ namespace TelCoColorCoder
     ColorPair GetColorFromPairNumber(int pairNumber) 
     {
         int zeroBasedPairNumber = pairNumber - 1;
-        MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-        MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        MajorColor majorColor = MajorColorNames[(zeroBasedPairNumber / numberOfMinorColors)];
+        MinorColor minorColor = MinorColorNames[(zeroBasedPairNumber % numberOfMinorColors)];
         return ColorPair(majorColor, minorColor);
     }
 
