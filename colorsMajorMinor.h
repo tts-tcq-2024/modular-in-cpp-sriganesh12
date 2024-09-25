0b84709509eec6a22a7fcf06da7239d30fad3bcb
sriganesh12 <49653366+sriganesh12@users.noreply.github.com>
2024-09-25 18:37:59 +0530

    Update colorsMajorMinor.h

--- colorsMajorMinor.h
+++ colorsMajorMinor.h
@@ -20,6 +20,9 @@ namespace TelCoColorCoder
     SLATE = 4
     };
 
+    const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
     const int numberOfMajorColors{5};
     const int numberOfMinorColors{5};
 }
